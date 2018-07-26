a78f7cc9540940907894c1c7737c03016a2e52fa
stenlee <lidan1811@gmail.com>
2018-07-26 14:29:53 +0200

    popView加入Animatet Sping动画。

--- ViewController.swift
+++ ViewController.swift
@@ -2,6 +2,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dimView: UIView!
+    
     @IBOutlet weak var tableView: UITableView!
     
     @IBOutlet var infoView: UIView!
@@ -10,10 +12,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var infoLabel: UILabel!
     
-    @IBAction func closeInfoPopup(_ sender: Any) {
-        infoView.removeFromSuperview()
-        
-    }
+   
     @IBOutlet weak var closeInfoPopupDesign: UIButton!
     var tableData:[ModleData]=[]
     
@@ -33,10 +32,29 @@ extension ViewController: UITableViewDataSource,UITableViewDelegate{
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let data = tableData[indexPath.row]
+        
         infoImage.image=data.image
         infoLabel.text=data.name
         infoView.center=view.center
+        
         view.addSubview(infoView)
+        infoView.transform=CGAffineTransform(scaleX: 0.8, y: 1.2)
+        
+        UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0, options: [], animations: {
+            self.dimView.alpha=0.8
+            self.infoView.transform = .identity
+        })
+    }
+    
+    @IBAction func closeInfoPopup(_ sender: Any) {
+        
+        
+        UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0, options: [], animations: {
+            self.dimView.alpha=0
+            self.infoView.transform=CGAffineTransform(scaleX: 0.2, y: 0.2)
+        }) { (succes) in
+            self.infoView.removeFromSuperview()
+        }
         
     }
     
@@ -48,6 +66,7 @@ extension ViewController: UITableViewDataSource,UITableViewDelegate{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return tableData.count
     }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell=tableView.dequeueReusableCell(withIdentifier: "cell") as! CharacterCell
         cell.setup(modelData: tableData[indexPath.row])
